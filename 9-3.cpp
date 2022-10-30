921b8e32f3b6bd4e5abe37b669c908a5b5b09a12
m-schwanekamp <112090940+m-schwanekamp@users.noreply.github.com>
2022-10-30 11:27:01 -0400

    Update 9-3.cpp

--- 9-3.cpp
+++ 9-3.cpp
@@ -11,18 +11,33 @@ int main() {
     int values[20], i;
 
     srand(time(0));
-    while (false) {
+    while (hasRun(values, 20) == false) {
+        cout << displayRun(values, 20) << " ";
+
         for(i = 0; i < 20; i++) {
         values[i] = rand() % 6 + 1;
+        
         cout << values[i] << " ";
         }
     }
     
 }
 void displayRun(int values[], int size) {
-
+    int values[20], i;
+    for(i = 0; i < 20; i++) {
+        values[i] = rand() % 6 + 1;
+    }
+    return values[i];
 }
 
 bool hasRun(int values[], int size) {
-    
+    if (values[0] != values[1] && values[2] != values[3] && values[4] != values[5]
+    && values[5] != values[6] && values[6] != values[7] && values[7] != values[8]
+    && values[8] != values[9] && values[9] != values[10] && values[10] != values[11]
+    && values[11] != values [12] && values[12] != values[13] && values[13] != values[14]
+    && values[14] != values[15] && values[15] != values[16] && values[16] != values[17]
+    && values[17] != values[18] && values[18] != values[19]) {
+        return false;
+    }
+    else return true;
 }
