6fef3164c31ce53749472c8d700b05b9a04a462e
m-schwanekamp <112090940+m-schwanekamp@users.noreply.github.com>
2022-11-01 23:10:56 -0400

    Update 9-3.cpp

--- 9-3.cpp
+++ 9-3.cpp
@@ -8,34 +8,38 @@ void displayRun(int values[], int size);
 bool hasRun(int values[], int size);
 
 int main() {
-    int values[20], i;
+    int integers[20], i;
 
     srand(time(0));
-    while (hasRun(values, 20) == false) {
+    while (hasRun(integers, 20) == false) {
         for (i = 0; i < 20; i++) {
-            values[i] = rand() % 6 + 1;
+            integers[i] = rand() % 6 + 1;
+            cout << integers[1] << " ";
         }
     }
     return 0;
 }
 
-void displayRun(int values[], int size) {
+/* void displayRun(int values[], int size) {
     int values[20], i;
+
     for(i = 0; i < 20; i++) {
         values[i] = rand() % 6 + 1;
         return values;
     }
     
 }
+*/
 
 bool hasRun(int values[], int size) {
-    if (values[0] != values[1] && values[2] != values[3] && values[4] != values[5]
-    && values[5] != values[6] && values[6] != values[7] && values[7] != values[8]
-    && values[8] != values[9] && values[9] != values[10] && values[10] != values[11]
-    && values[11] != values [12] && values[12] != values[13] && values[13] != values[14]
-    && values[14] != values[15] && values[15] != values[16] && values[16] != values[17]
-    && values[17] != values[18] && values[18] != values[19]) {
+    if (values[0] != values[1] || values[2] != values[3] || values[4] != values[5]
+    || values[5] != values[6] || values[6] != values[7] || values[7] != values[8]
+    || values[8] != values[9] || values[9] != values[10] || values[10] != values[11]
+    || values[11] != values [12] || values[12] != values[13] || values[13] != values[14]
+    || values[14] != values[15] || values[15] != values[16] || values[16] != values[17]
+    || values[17] != values[18] || values[18] != values[19]) {
         return false;
     }
     else return true;
+
 }
